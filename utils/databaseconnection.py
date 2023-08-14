f51348de96420de6a0c067ac5e510d246052524b
Your Name <you@example.com>
2023-08-14 11:53:07 +0200

    error-handling

--- utils/databaseconnection.py
+++ utils/databaseconnection.py
@@ -9,6 +9,9 @@ class Databaseconnection():
         self.connection = sqlite3.connect(self.host)
         return self.connection
     
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.connection.commit()
-        self.connection.close()
+    def __exit__(self, exc_type, exc_val, exc_tb): 
+        if  exc_type or exc_val or  exc_tb:
+            self.connection.close()
+        else:
+            self.connection.commit()
+            self.connection.close()
